aa7232d532b31e8769b152a6877a88f3c7dbcf36
Jemerick Castillo <jemerick.castillo@utp.ac.pa>
2020-06-18 10:52:26 -0500

    animation bar js

---
+++ bar-animation-home.js
@@ -0,0 +1,321 @@
+jQuery(document).ready(function() {
+
+    $('.bar-animated-1').hover(
+
+        function() {
+        $('.text-bar-1').addClass( "animation1" );
+        $('.text-bar-animated').addClass( "position" );
+        $('.text-bar-1').addClass( "show-element fade-in" );
+
+        //Add classes
+        $('.text-bar-2').addClass( "hide-element" );
+        $('.text-bar-3').addClass( "hide-element" );
+        $('.text-bar-4').addClass( "hide-element" );
+        $('.text-bar-5').addClass( "hide-element" );
+        $('.text-bar-6').addClass( "hide-element" );
+        $('.text-bar-7').addClass( "hide-element" );
+
+
+        //Remove classes
+        $('.text-bar-1').removeClass( "animation2" );
+        $('.text-bar-3').removeClass( "animation3" );
+        $('.text-bar-4').removeClass( "animation4" );
+        $('.text-bar-5').removeClass( "animation5" );
+        $('.text-bar-6').removeClass( "animation6" );
+        $('.text-bar-7').removeClass( "animation7" );
+
+        //Remove classes
+        $('.text-bar-2').removeClass( "show-element fade-in" );
+        $('.text-bar-3').removeClass( "show-element fade-in" );
+        $('.text-bar-4').removeClass( "show-element fade-in" );
+        $('.text-bar-5').removeClass( "show-element fade-in" );
+        $('.text-bar-6').removeClass( "show-element fade-in" );
+        $('.text-bar-7').removeClass( "show-element fade-in" );
+
+        //Background
+        $('._1332').addClass("back1");
+        
+        //Remove other background classes
+        $('._1332').removeClass("back2");
+        $('._1332').removeClass("back3");
+        $('._1332').removeClass("back4");
+        $('._1332').removeClass("back5");
+        $('._1332').removeClass("back6");
+        $('._1332').removeClass("back7");
+
+
+        }
+   
+    );
+
+    $('.bar-animated-2').hover(
+        function() {
+            $('.text-bar-2').addClass( "animation2" );
+            $('.text-bar-animated').addClass( "position" );
+            $('.text-bar-2').addClass( "show-element fade-in" );
+
+            //Add classes
+            $('.text-bar-1').addClass( "hide-element" );
+            $('.text-bar-3').addClass( "hide-element" );
+            $('.text-bar-4').addClass( "hide-element" );
+            $('.text-bar-5').addClass( "hide-element" );
+            $('.text-bar-6').addClass( "hide-element" );
+            $('.text-bar-7').addClass( "hide-element" );
+
+            //Remove classes
+            $('.text-bar-1').removeClass( "animation1" );
+            $('.text-bar-3').removeClass( "animation3" );
+            $('.text-bar-4').removeClass( "animation4" );
+            $('.text-bar-5').removeClass( "animation5" );
+            $('.text-bar-6').removeClass( "animation6" );
+            $('.text-bar-7').removeClass( "animation7" );
+
+            //Remove classes
+            $('.text-bar-1').removeClass( "show-element fade-in" );
+            $('.text-bar-3').removeClass( "show-element fade-in" );
+            $('.text-bar-4').removeClass( "show-element fade-in" );
+            $('.text-bar-5').removeClass( "show-element fade-in" );
+            $('.text-bar-6').removeClass( "show-element fade-in" );
+            $('.text-bar-7').removeClass( "show-element fade-in" );
+
+            //Background
+            $('._1332').addClass("back2");
+            
+            //Remove other background classes
+            $('._1332').removeClass("back1");
+            $('._1332').removeClass("back3");
+            $('._1332').removeClass("back4");
+            $('._1332').removeClass("back5");
+            $('._1332').removeClass("back6");
+            $('._1332').removeClass("back7");
+
+        }
+    );
+
+
+    $('.bar-animated-3').hover(
+        function() {
+                $('.text-bar-3').addClass( "animation3" );
+                $('.text-bar-animated').addClass( "position" );
+                $('.text-bar-3').addClass( "show-element fade-in" );
+
+                //Add classes
+                $('.text-bar-1').addClass( "hide-element" );
+                $('.text-bar-2').addClass( "hide-element" );
+                $('.text-bar-4').addClass( "hide-element" );
+                $('.text-bar-5').addClass( "hide-element" );
+                $('.text-bar-6').addClass( "hide-element" );
+                $('.text-bar-7').addClass( "hide-element" );
+
+                //Remove classes
+                $('.text-bar-1').removeClass( "animation1" );
+                $('.text-bar-2').removeClass( "animation2" );
+                $('.text-bar-4').removeClass( "animation4" );
+                $('.text-bar-5').removeClass( "animation5" );
+                $('.text-bar-6').removeClass( "animation6" );
+                $('.text-bar-7').removeClass( "animation7" );
+
+                
+                //Remove classes
+                $('.text-bar-1').removeClass( "show-element fade-in" );
+                $('.text-bar-2').removeClass( "show-element fade-in" );
+                $('.text-bar-4').removeClass( "show-element fade-in" );
+                $('.text-bar-5').removeClass( "show-element fade-in" );
+                $('.text-bar-6').removeClass( "show-element fade-in" );
+                $('.text-bar-7').removeClass( "show-element fade-in" );
+
+                //Background
+                $('._1332').addClass("back3");
+                
+                //Remove other background classes
+                $('._1332').removeClass("back1");
+                $('._1332').removeClass("back2");
+                $('._1332').removeClass("back4");
+                $('._1332').removeClass("back5");
+                $('._1332').removeClass("back6");
+                $('._1332').removeClass("back7");
+            }
+    );
+
+
+    $('.bar-animated-4').hover(
+        function() {
+                $('.text-bar-4').addClass( "animation4" );
+                $('.text-bar-animated').addClass( "position" );
+                $('.text-bar-4').addClass( "show-element fade-in" );
+
+                //Add classes
+                $('.text-bar-1').addClass( "hide-element" );
+                $('.text-bar-3').addClass( "hide-element" );
+                $('.text-bar-2').addClass( "hide-element" );
+                $('.text-bar-5').addClass( "hide-element" );
+                $('.text-bar-6').addClass( "hide-element" );
+                $('.text-bar-7').addClass( "hide-element" );
+
+                //Remove classes
+                $('.text-bar-1').removeClass( "animation1" );
+                $('.text-bar-2').removeClass( "animation2" );
+                $('.text-bar-3').removeClass( "animation3" );
+                $('.text-bar-5').removeClass( "animation5" );
+                $('.text-bar-6').removeClass( "animation6" );
+                $('.text-bar-7').removeClass( "animation7" );
+
+                
+                //Remove classes
+                $('.text-bar-1').removeClass( "show-element fade-in" );
+                $('.text-bar-3').removeClass( "show-element fade-in" );
+                $('.text-bar-2').removeClass( "show-element fade-in" );
+                $('.text-bar-5').removeClass( "show-element fade-in" );
+                $('.text-bar-6').removeClass( "show-element fade-in" );
+                $('.text-bar-7').removeClass( "show-element fade-in" );
+
+
+                //Background
+                $('._1332').addClass("back4");
+        
+                //Remove other background classes
+                $('._1332').removeClass("back1");
+                $('._1332').removeClass("back2");
+                $('._1332').removeClass("back3");
+                $('._1332').removeClass("back5");
+                $('._1332').removeClass("back6");
+                $('._1332').removeClass("back7");
+            }
+    );
+
+    $('.bar-animated-5').hover(
+        function() {
+                $('.text-bar-5').addClass( "animation5" );
+                $('.text-bar-animated').addClass( "position" );
+                $('.text-bar-5').addClass( "show-element fade-in" );
+
+                //Add classes
+                $('.text-bar-1').addClass( "hide-element" );
+                $('.text-bar-3').addClass( "hide-element" );
+                $('.text-bar-4').addClass( "hide-element" );
+                $('.text-bar-2').addClass( "hide-element" );
+                $('.text-bar-6').addClass( "hide-element" );
+                $('.text-bar-7').addClass( "hide-element" );
+
+                //Remove classes
+                $('.text-bar-1').removeClass( "animation1" );
+                $('.text-bar-2').removeClass( "animation2" );
+                $('.text-bar-3').removeClass( "animation3" );
+                $('.text-bar-4').removeClass( "animation4" );
+                $('.text-bar-6').removeClass( "animation6" );
+                $('.text-bar-7').removeClass( "animation7" );
+
+                
+                //Remove classes
+                $('.text-bar-1').removeClass( "show-element fade-in");
+                $('.text-bar-3').removeClass( "show-element fade-in");
+                $('.text-bar-4').removeClass( "show-element fade-in");
+                $('.text-bar-2').removeClass( "show-element fade-in");
+                $('.text-bar-6').removeClass( "show-element fade-in");
+                $('.text-bar-7').removeClass( "show-element fade-in");
+
+                //Background
+                $('._1332').addClass("back5");
+                            
+                //Remove other background classes
+                $('._1332').removeClass("back1");
+                $('._1332').removeClass("back2");
+                $('._1332').removeClass("back4");
+                $('._1332').removeClass("back3");
+                $('._1332').removeClass("back6");
+                $('._1332').removeClass("back7");
+            }
+    );
+
+    $('.bar-animated-6').hover(
+        function() {
+                $('.text-bar-6').addClass( "animation6" );
+                $('.text-bar-animated').addClass( "position" );
+                $('.text-bar-6').addClass( "show-element fade-in" );
+
+                //Add classes
+                $('.text-bar-1').addClass( "hide-element" );
+                $('.text-bar-3').addClass( "hide-element" );
+                $('.text-bar-4').addClass( "hide-element" );
+                $('.text-bar-5').addClass( "hide-element" );
+                $('.text-bar-2').addClass( "hide-element" );
+                $('.text-bar-7').addClass( "hide-element" );
+
+                //Remove classes
+                $('.text-bar-1').removeClass( "animation1" );
+                $('.text-bar-2').removeClass( "animation2" );
+                $('.text-bar-3').removeClass( "animation3" );
+                $('.text-bar-4').removeClass( "animation4" );
+                $('.text-bar-5').removeClass( "animation5" );
+                $('.text-bar-7').removeClass( "animation7" );
+
+                
+                //Remove classes
+                $('.text-bar-1').removeClass( "show-element fade-in");
+                $('.text-bar-3').removeClass( "show-element fade-in");
+                $('.text-bar-4').removeClass( "show-element fade-in");
+                $('.text-bar-5').removeClass( "show-element fade-in");
+                $('.text-bar-2').removeClass( "show-element fade-in");
+                $('.text-bar-7').removeClass( "show-element fade-in");
+
+                //Background
+                $('._1332').addClass("back6");
+            
+                //Remove other background classes
+                $('._1332').removeClass("back1");
+                $('._1332').removeClass("back2");
+                $('._1332').removeClass("back4");
+                $('._1332').removeClass("back3");
+                $('._1332').removeClass("back5");
+                $('._1332').removeClass("back7");
+
+            }
+    );
+
+    $('.bar-animated-7').hover(
+        function() {
+                $('.text-bar-7').addClass( "animation7" );
+                $('.text-bar-animated').addClass( "position" );
+                $('.text-bar-7').addClass( "show-element fade-in" );
+
+                //Add classes
+                $('.text-bar-1').addClass( "hide-element" );
+                $('.text-bar-3').addClass( "hide-element" );
+                $('.text-bar-4').addClass( "hide-element" );
+                $('.text-bar-5').addClass( "hide-element" );
+                $('.text-bar-6').addClass( "hide-element" );
+                $('.text-bar-2').addClass( "hide-element" );
+
+                //Remove classes
+                $('.text-bar-1').removeClass( "animation1" );
+                $('.text-bar-2').removeClass( "animation2" );
+                $('.text-bar-3').removeClass( "animation3" );
+                $('.text-bar-4').removeClass( "animation4" );
+                $('.text-bar-5').removeClass( "animation5" );
+                $('.text-bar-6').removeClass( "animation6" );
+
+                
+                //Remove classes
+                $('.text-bar-1').removeClass( "show-element fade-in");
+                $('.text-bar-3').removeClass( "show-element fade-in");
+                $('.text-bar-4').removeClass( "show-element fade-in");
+                $('.text-bar-5').removeClass( "show-element fade-in");
+                $('.text-bar-6').removeClass( "show-element fade-in");
+                $('.text-bar-2').removeClass( "show-element fade-in");
+
+                //Background
+                $('._1332').addClass("back7");
+
+                //Remove other background classes
+                $('._1332').removeClass("back1");
+                $('._1332').removeClass("back2");
+                $('._1332').removeClass("back4");
+                $('._1332').removeClass("back3");
+                $('._1332').removeClass("back5");
+                $('._1332').removeClass("back6");
+
+
+            }
+    );
+    
+});
