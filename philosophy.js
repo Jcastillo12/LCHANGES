be6e00515349074043de271f72a8baef753e4b71
Jemerick Castillo <jemerick.castillo@utp.ac.pa>
2020-07-08 17:07:49 -0500

    cambios

---
+++ philosophy.js
@@ -0,0 +1,74 @@
+jQuery(document).ready(function() {
+
+    $( "#we_x5F_do_x5F_it_x5F_the_x5F_best_x5F_box" ).hover(function() {
+        $( "#we_x5F_do_x5F_it_x5F_the_x5F_best" ).toggleClass("changeorange ");
+    });
+
+    $( "#our_x5F_work_x5F_has_x5F_box" ).hover(function() {
+        $("#our_x5F_work_x5F_has_x5F_purpose").toggleClass("changeblue ");
+    });
+
+    $( "#when_x5F_we_x5F_collaborate_x5F_box" ).hover(function() {
+        $("#when_x5F_we_x5F_collaborate").toggleClass("changered ");
+    });
+
+    $( "#we_x5F_own_x5F_it_x5F_box" ).hover(function() {
+        $("#we_x5F_own_x5F_it").toggleClass("changegreen ");
+    });
+
+    $( "#if_x5F_we_x5F_dont_x5F_have_x5F_box" ).hover(function() {
+        $("#if_x5F_we_x5F_dont_x5F_have").toggleClass("changepurple ");
+    });
+
+    $( "#growth_x5F_in_x5F_our_x5F_markets_x5F_box" ).hover(function() {
+        $("#growth_x5F_in_x5F_our_x5F_markets").toggleClass("changegray ");
+    });
+
+    $( "#kindness_x5F_is_x5F_not_x5F_box" ).hover(function() {
+        $("#kindness_x5F_is_x5F_not").toggleClass("changeyellow ");
+    });
+
+    $( "#we_x5F_are_x5F_considerate_x5F_box" ).hover(function() {
+        $("#we_x5F_are_x5F_considerate_x5F_of_x5F_others").toggleClass("changebrown ");
+    });
+
+    $( "#we_x5F_are_x5F_accountable_x5F_box" ).hover(function() {
+        $("#we_x5F_are_x5F_accountable").toggleClass("changepink ");
+    });
+
+    $( "#we_x5F_are_x5F_personable_x5F_box" ).hover(function() {
+        $("#we_x5F_are_x5F_personable").toggleClass("changeorange2");
+    });
+
+    $( "#we_x5F_are_x5F_courageous_x5F_box" ).hover(function() {
+        $("#we_x5F_are_x5F_courageous").toggleClass("changeblue2");
+    });
+
+    $( "#passion_x5F_in_x5F_all_x5F_box" ).hover(function() {
+        $("#passion_x5F_in_x5F_all").toggleClass("changered2");
+    });
+
+    $( "#our_x5F_diversity_x5F_define_x5F_box").hover(function() {
+        $("#our_x5F_diversity_x5F_define").toggleClass("changegreen2");
+    });
+
+    $( "#technology_x5F_excites_x5F_us_x5F_box").hover(function() {
+        $("#technology_x5F_excites_x5F_us").toggleClass("changepurple2");
+    });
+
+    $( "#we_x5F_are_x5F_inspired_x5F_box").hover(function() {
+        $("#we_x5F_are_x5F_inspired").toggleClass("changegray2");
+    });
+
+    $( "#heart_x5F_is_x5F_more_x5F_box").hover(function() {
+        $("#heart_x5F_is_x5F_more").toggleClass("changeyellow2");
+    });
+
+    
+    
+    
+    
+
+
+});
+
